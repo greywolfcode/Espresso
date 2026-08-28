162ee68d2c4f9032a354de6a298e786d0b496a9a
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-08-28 16:38:46 -0400

    make HeaderInserter store file data in HashMap

--- src-devtools/HeaderInserter.java
+++ src-devtools/HeaderInserter.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class HeaderInserter 
@@ -18,6 +19,7 @@ public class HeaderInserter
     private static void process(String folderPath)
     {
         Stack<File> folders = new Stack<File>();
+        HashMap<String, FileStorage> files = new HashMap<String, FileStorage>();
         File toplevel = new File(folderPath);
         folders.add(toplevel);
 
@@ -29,10 +31,16 @@ public class HeaderInserter
 
             for (File file : currentFolder.listFiles(File::isFile))
             {
-               if (getExtension(file).equals(".java"))
-               {
-                
-               }
+                if (getExtension(file).equals(".java"))
+                {
+                    String name = getName(file);
+                    //remove the top level path to just get the package path
+                    String packageName = file.getPath().substring(toplevel.getPath().length());
+
+                    FileStorage currentFile = new FileStorage(name, packageName, file);
+
+                    files.put(name, currentFile);
+                }
             }
         }
 
@@ -54,4 +62,47 @@ public class HeaderInserter
 
         return name.substring(extensionIndex);
     }
+    private static String getName(File file)
+    {
+        if (file == null)
+        {
+            return "";
+        }
+
+        String name = file.getName();
+        int extensionIndex = name.lastIndexOf(".");
+
+        if (extensionIndex == -1)
+        {
+            return "";
+        }
+
+        return name.substring(0, extensionIndex);
+    }
 }
+
+class FileStorage
+    {
+        String name;
+        String packageName;
+        File file;
+
+        public FileStorage(String paramName, String paramPackageName, File paramFile)
+        {
+            name = paramName;
+            packageName = paramPackageName;
+            file = paramFile;
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public String getPackagename()
+        {
+            return packageName;
+        }
+        public File getFile()
+        {
+            return file;
+        }
+    }
