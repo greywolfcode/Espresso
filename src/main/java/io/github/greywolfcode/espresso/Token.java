39b0f4b74d0ddf6090db22f08060c12e01d9d87b
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-08-30 22:16:00 -0400

    Update Token class

--- src/main/java/io/github/greywolfcode/espresso/Token.java
+++ src/main/java/io/github/greywolfcode/espresso/Token.java
@@ -1,9 +1,23 @@
 package io.github.greywolfcode.espresso;
 
+import io.github.greywolfcode.espresso.TokenType;
+
 public class Token 
 {
     private int offset;
-    private int length;
     private String file;
+    private String token;
+    private TokenType type;
+
+    public Token(int paramOffset, String paramFile, String paramToken, TokenType type)
+    {
+        offset = paramOffset;
+        file = paramFile;
+        token = paramToken;
+    }
+    public String toString()
+    {
+        return "Token from "
+    }
 
 }
