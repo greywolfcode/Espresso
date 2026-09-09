4ed9d1190540032e7f52672cced6f56314fc5d8a
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-09-09 15:25:51 -0400

    Create LineData record for storing error info

---
+++ src/main/java/io/github/greywolfcode/espresso/errorreporting/LineData.java
@@ -0,0 +1,22 @@
+/*
+ * Espresso Compiler
+ * Copyright (C) 2026  greywolfcode
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package io.github.greywolfcode.espresso.errorreporting;
+
+//offset is offset into the line, not offset from file
+public record LineData (int lineNum, int offset, String line) {}
